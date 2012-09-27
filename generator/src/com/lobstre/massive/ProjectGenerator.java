8a91e40b44cedf7033ad0465055d2dce3b197b60
Florent Le Gall <florent.legall@gmail.com>
2012-09-27 19:33:27 +0200

    Added java code generation + classpath & project templates

--- generator/src/com/lobstre/massive/ProjectGenerator.java
+++ generator/src/com/lobstre/massive/ProjectGenerator.java
@@ -1,16 +1,27 @@
 package com.lobstre.massive;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.text.DecimalFormat;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ProjectGenerator {
 
@@ -91,17 +102,21 @@ public class ProjectGenerator {
 		final File productDir = makeSubDir(companyDir, "product");
 		final File projectPackageDir = makeSubDir(productDir, projectName);
 		for (int i = 0; i < 10; i++) {
-			final File packageDir = makeSubDir(projectPackageDir, "package" + i);
+			final String packageName = "package" + i;
+			final File packageDir = makeSubDir(projectPackageDir, packageName);
 			for (int j = 0; j < 10; j++) {
-				final File subPackageDir = makeSubDir(packageDir, "subpackage" + j);
+				final String subPackageName = "subpackage" + j;
+				final File subPackageDir = makeSubDir(packageDir, subPackageName);
 				for (int k = 0; k < 10; k++) {
 					InputStream resource = null;
 					FileOutputStream fos = null;
+					File javaFile = null;
+					final String className = "UberClass_" + projectName + df.format(i) + df.format(j) + df.format(k);
 					try {
 						final ClassLoader cl = ProjectGenerator.class.getClassLoader();
 						resource = cl.getResourceAsStream("com/lobstre/massive/ClassTemplate.txt");
-						final String fileName = "UberClass_" + projectName + df.format(i) + df.format(j) + df.format(k) + ".java";
-						final File javaFile = new File (subPackageDir, fileName);
+						final String fileName = className + ".java";
+						javaFile = new File (subPackageDir, fileName);
 						fos = new FileOutputStream (javaFile);
 						copy (resource, fos);
 					} finally {
@@ -116,7 +131,10 @@ public class ProjectGenerator {
 						}
 						
 					}
-					
+					rewrite (javaFile, ProjectGenerator.<String, String>asMap(new Object[][] {
+							{"CLASS_NAME", className},
+							{"PACKAGE_NAME", "com.company.product."+packageName + "." +subPackageName},
+					}));
 				}
 			}
 		}
@@ -129,6 +147,7 @@ public class ProjectGenerator {
 	}
 	
     private static final int BUFFER_SIZE = 4096;
+	private static final String UTF8_CHARSET = "UTF-8";
     private static ThreadLocal<byte[]> buffers = new ThreadLocal<byte[]>() {
 
         @Override
@@ -146,5 +165,46 @@ public class ProjectGenerator {
             outputStream.write(buffer, 0, readBytes);
         }
     }
+    
+    private static void rewrite (final File valuesFile, final Map<String, String> replacements) throws IOException {
+        String file = readWholeFile (valuesFile);
+        for (final Map.Entry<String, String> e : replacements.entrySet ()) {
+            final Pattern pattern = Pattern.compile (e.getKey (), Pattern.LITERAL);
+            final String replacementQuoted = Matcher.quoteReplacement (e.getValue ());
+            final Matcher matcher = pattern.matcher (file);
+            file = matcher.replaceAll (replacementQuoted);
+        }
+        writeFile (valuesFile, file);
+    }
+
+    private static String readWholeFile (final File file) throws IOException {
+        final BufferedReader reader = new BufferedReader (new InputStreamReader (new FileInputStream (file), UTF8_CHARSET));
+        String line = null;
+        final StringBuilder stringBuilder = new StringBuilder ();
+        final String ls = System.getProperty ("line.separator");
+        while ((line = reader.readLine ()) != null) {
+            stringBuilder.append (line);
+            stringBuilder.append (ls);
+        }
+        reader.close ();
+        return stringBuilder.toString ();
+    }
+
+    private static void writeFile (final File valuesFile, final String file) throws FileNotFoundException, UnsupportedEncodingException {
+        final PrintWriter pw = new PrintWriter (valuesFile, UTF8_CHARSET);
+        pw.write (file);
+        pw.close ();
+    }    
+    
+	@SuppressWarnings("unchecked")
+    private static <K, V> Map<K, V> asMap (final Object [][] in) {
+	    final Map<K, V> ret = new LinkedHashMap <K, V> (in.length);
+	    
+	    for (Object[] keyValue : in) {
+	        ret.put ((K)keyValue[0], (V)keyValue[1]);
+	    }
+
+	    return Collections.unmodifiableMap (ret);
+	}    
     
 }
